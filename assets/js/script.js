c133484cb9eca739a8cde562c9f2a126776f3726
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-04-11 20:29:36 +0300

    add button "return back" on schedule page and add progress bar on main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,14 @@
-/*  clock */
-const hours = document.querySelector(".hours");
-const minutes = document.querySelector(".minutes");
-const seconds = document.querySelector(".seconds");
+$(document).ready(function () {
+  const progressBar = document.getElementById("progressBar");
+  const progressText = document.getElementById("progressText");
 
-clock = () => {
-  let today = new Date();
-  let h = (today.getHours() % 12) + today.getMinutes() / 59; // 22 % 12 = 10pm
-  let m = today.getMinutes(); // 0 - 59
-  let s = today.getSeconds(); // 0 - 59
+  function setProgress(heightPercentage) {
+    progressBar.style.height = heightPercentage + "%";
+    progressText.textContent = heightPercentage + "%";
+  }
 
-  h *= 30; // 12 * 30 = 360deg
-  m *= 6;
-  s *= 6; // 60 * 6 = 360deg
-
-  rotation(hours, h);
-  rotation(minutes, m);
-  rotation(seconds, s);
-
-  // call every second
-  setTimeout(clock, 500);
-};
-
-rotation = (target, val) => {
-  target.style.transform = `rotate(${val}deg)`;
-};
-
-window.onload = clock();
+  setProgress(90);
+});
 
 function toggleDiv() {
   $(".components").toggle();
